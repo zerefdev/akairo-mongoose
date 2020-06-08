a3a42ee00e039b2864a58b35362aa4125701990b
Zerefdev <mosseab.chakrani@gmail.com>
2020-06-08 18:39:51 +0200

    try catch, 4 spaces for better readability

--- index.js
+++ index.js
@@ -6,120 +6,118 @@ const { Provider } = require('discord-akairo');
  * @extends {Provider}
  */
 class MongooseProvider extends Provider {
-  constructor(model) {
-    super();
+    constructor(model) {
+        super();
+
+        /**
+         * Mongoose model.
+         * @type {Model}
+         */
+        this.model = model;
+    }
 
     /**
-     * Mongoose model.
-     * @type {Model}
+     * Initializes the provider.
+     * @returns {<void>}
      */
-    this.model = model;
-  }
-
-  /**
-   * Initializes the provider.
-   * @returns {<void>}
-   */
-  async init() {
-    try {
-    const guilds = await this.model.find();
-    for (const i in guilds) {
-      const guild = guilds[i];
-      this.items.set(guild.id, guild.settings);
-    }
-    } catch (error) {
-    console.error(error);
-    }
-  }
-
-  /**
-   * Gets a value.
-   * @param {string} id - guildID.
-   * @param {string} key - The key to get.
-   * @param {any} [defaultValue] - Default value if not found or null.
-   * @returns {any}
-   */
-  get(id, key, defaultValue) {
-    if (this.items.has(id)) {
-      const value = this.items.get(id)[key];
-      return value == null ? defaultValue : value;
+    async init() {
+        try {
+            const guilds = await this.model.find();
+            for (const i in guilds) {
+                const guild = guilds[i];
+                this.items.set(guild.id, guild.settings);
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    return defaultValue;
-  }
+    /**
+     * Gets a value.
+     * @param {string} id - guildID.
+     * @param {string} key - The key to get.
+     * @param {any} [defaultValue] - Default value if not found or null.
+     * @returns {any}
+     */
+    get(id, key, defaultValue) {
+        if (this.items.has(id)) {
+            const value = this.items.get(id)[key];
+            return value == null ? defaultValue : value;
+        }
+        return defaultValue;
+    }
 
-  /**
-   * Sets a value.
-   * @param {string} id - guildID.
-   * @param {string} key - The key to set.
-   * @param {any} value - The value.
-   * @returns {Promise} - Mongoose query object|document
-   */
-  async set(id, key, value) {
-    const data = this.items.get(id) || {};
-    data[key] = value;
-    this.items.set(id, data);
-    try {
-      const doc = await this.getDocument(id);
-      doc.settings[key] = value;
-      doc.markModified('settings');
-      return await doc.save();
-    } catch (error) {
-      console.error(error);
+    /**
+     * Sets a value.
+     * @param {string} id - guildID.
+     * @param {string} key - The key to set.
+     * @param {any} value - The value.
+     * @returns {Promise} - Mongoose query object|document
+     */
+    async set(id, key, value) {
+        const data = this.items.get(id) || {};
+        data[key] = value;
+        this.items.set(id, data);
+        try {
+            const doc = await this.getDocument(id);
+            doc.settings[key] = value;
+            doc.markModified('settings');
+            return await doc.save();
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  /**
-   * Deletes a value.
-   * @param {string} id - guildID.
-   * @param {string} key - The key to delete.
-   * @returns {Promise} - Mongoose query object|document
-   */
-  async delete(id, key) {
-    const data = this.items.get(id) || {};
-    delete data[key];
-    try {
-    const doc = await this.getDocument(id);
-    delete doc.settings[key];
-    doc.markModified('settings');
-    return await doc.save();
-    } catch (error) {
-     console.error(error);
+    /**
+     * Deletes a value.
+     * @param {string} id - guildID.
+     * @param {string} key - The key to delete.
+     * @returns {Promise} - Mongoose query object|document
+     */
+    async delete(id, key) {
+        const data = this.items.get(id) || {};
+        delete data[key];
+        try {
+            const doc = await this.getDocument(id);
+            delete doc.settings[key];
+            doc.markModified('settings');
+            return await doc.save();
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  /**
-   * Removes a document.
-   * @param {string} id - GuildID.
-   * @returns {Promise<void>}
-   */
-  async clear(id) {
-    this.items.delete(id);
-    const doc = await this.getDocument(id);
-    try {
-    if (doc) await doc.remove();
-    } catch (error) {
-      console.error(error);
+    /**
+     * Removes a document.
+     * @param {string} id - GuildID.
+     * @returns {Promise<void>}
+     */
+    async clear(id) {
+        this.items.delete(id);
+        try {
+            const doc = await this.getDocument(id);
+            return await doc.remove();
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  /**
-   * Gets a document by guildID.
-   * @param {string} id - guildID.
-   * @returns {Promise} - Mongoose query object|document
-   */
-  async getDocument(id) {
-    try {
-    const obj = await this.model.findOne({ id });
-    if (!obj) {
-      const newDoc = await new this.model({ id, settings: {} }).save();
-      return newDoc;
+    /**
+     * Gets a document by guildID.
+     * @param {string} id - guildID.
+     * @returns {Promise} - Mongoose query object|document
+     */
+    async getDocument(id) {
+        try {
+            const obj = await this.model.findOne({ id });
+            if (!obj) {
+                obj = await new this.model({ id, settings: {} }).save();
+            }
+            return obj;
+        } catch (error) {
+            console.error(error);
+        }
     }
-    return obj;
-    } catch (error) {
-      console.error(error);
-    }
-  }
 }
 
 module.exports = MongooseProvider;
